14d0b117f30c294b4409a19df05b244732040950
nitinkumar36 <68391330+nitinkumar36@users.noreply.github.com>
2021-10-11 23:38:50 -0400

    Initial Commit

---
+++ src/main/java/com/deloitte/accelerator/web/ApiConfig.java
@@ -0,0 +1,46 @@
+package com.deloitte.accelerator.web;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class ApiConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .build().securitySchemes(basicScheme());
+    }
+
+    private List<SecurityScheme> basicScheme() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new BasicAuth("basicAuth"));
+        return schemeList;
+    }
+
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder()
+                .title( "Spring Boot Template Initializer " )
+                .description( "All the sample requests for the SB Initializer" )
+                .version( "1.0.0" )
+                .build();
+    }
+}
+
